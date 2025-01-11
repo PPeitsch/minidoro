a951a08e0abd613e52f7d766e6d6a743836b4f1c
PabloPeitsch <pablo.peitsch@gmail.com>
2025-01-11 20:21:22 -0300

    style: Format code with isort and black
    - Sort imports using isort
    - Apply consistent code formatting with black
    - Format Python files according to PEP 8

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
-from flask import Flask
 import os
 
-app = Flask(__name__, static_folder=os.path.abspath('app/static'))
+from flask import Flask
+
+app = Flask(__name__, static_folder=os.path.abspath("app/static"))
 
 from app import routes
